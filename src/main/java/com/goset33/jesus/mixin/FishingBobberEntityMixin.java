169532d45b0da0d58aa964451370f7b5fdb3433f
goset33 <gogaprofi09@gmail.com>
2024-09-14 01:04:05 +0700

    Fishing magic
    - Now when you throw a fishing rod, a fish will be caught immediately
    - You will no longer be able to catch junk when fishing
    - Fish or treasure will be caught 3-5 at a time

---
+++ src/main/java/com/goset33/jesus/mixin/FishingBobberEntityMixin.java
@@ -0,0 +1,68 @@
+package com.goset33.jesus.mixin;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import net.minecraft.entity.ItemEntity;
+import net.minecraft.entity.projectile.FishingBobberEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.loot.LootTable;
+import net.minecraft.loot.LootTables;
+import net.minecraft.loot.context.LootContextParameterSet;
+import net.minecraft.text.Text;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@Mixin(FishingBobberEntity.class)
+public abstract class FishingBobberEntityMixin {
+    @Accessor("waitCountdown")
+    public abstract void setWaitCountdown(int waitCountdown);
+
+    @Inject(method = "tickFishingLogic(Lnet/minecraft/util/math/BlockPos;)V", at = @At("HEAD"))
+    private void InstantFish(BlockPos pos, CallbackInfo info) {
+        setWaitCountdown(1);
+    }
+
+    @Inject(method = "use(Lnet/minecraft/item/ItemStack;)I", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;spawnEntity(Lnet/minecraft/entity/Entity;)Z"))
+    private void MultiplyDrop(ItemStack usedItem, CallbackInfoReturnable<Integer> info, @Local ItemEntity itemEntity, @Local LocalRef<ItemEntity> localRef, @Local LootContextParameterSet lootContextParameterSet, @Local LootTable lootTable) {
+        ItemStack containStack = itemEntity.getStack();
+        ArrayList<String> junks = new ArrayList<>(Arrays.asList(
+                "lily_pad",
+                "bowl",
+                "fishing_rod",
+                "leather",
+                "leather_boots",
+                "rotten_flesh",
+                "stick",
+                "string",
+                "potion",
+                "bone",
+                "ink_sac",
+                "tripwire_hook"
+        ));
+
+        for (String item : junks) {
+            while (containStack.getItem().toString().equals(item)) {
+                lootTable = ((FishingBobberEntity) (Object) this).getWorld().getServer().getLootManager().getLootTable(LootTables.FISHING_GAMEPLAY);
+                ObjectArrayList<ItemStack> list = lootTable.generateLoot(lootContextParameterSet);
+                for (ItemStack itemStack : list) {
+                    itemStack.setCustomName(Text.of("Changed."));
+                    containStack = itemStack;
+                }
+            }
+        }
+
+        containStack.increment(Random.create().nextBetween(1, 2));
+        itemEntity.setStack(containStack);
+        localRef.set(itemEntity);
+    }
+}
